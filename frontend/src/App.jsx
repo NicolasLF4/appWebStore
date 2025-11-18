98a37d2a894da6b2785d4599a4037f93b6bbcad3
Gonzalo Silvero <gonzalosilvero02@gmail.com>
2025-11-18 02:25:42 -0300

    Agrego rutas para estructura de directorios y principio de pruebas de CRUD en la HomePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
+import { HomePage } from './pages/HomePage/HomePage'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
+      <HomePage/>
+      
 
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
 
-      <h1>Vite + React</h1>
+      {/* <h1>Vite + React</h1>
 
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -31,7 +24,7 @@ function App() {
 
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
 
     </>
   )
