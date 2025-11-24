3ceebd0ae718a8f107ae727748a4004dca20dd9b
Gonzalo Silvero <gonzalosilvero02@gmail.com>
2025-11-24 13:14:04 -0300

    Difuminado al carrusel

--- frontend/src/pages/HomePage/HomeCarousel.jsx
+++ frontend/src/pages/HomePage/HomeCarousel.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import Carousel from 'react-bootstrap/Carousel';
-import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "../../styles/HomeCarousel.module.css";
 import img1 from "../../assets/img1.png";
 import img2 from "../../assets/img2.jpg";
 
 export const HomeCarousel = () => {
     return (
-        <div style={{ justifyItems: "center" }}>
-            <Carousel style={{ width: "60%"}}>
+        // <div style={{ justifyItems: "center" }}>
+            <Carousel style={{ width: "100%"}}>
                 <Carousel.Item interval={5000}>
-                    <img src={img1} className={ styles.item }/>
+                    <div className={ styles.item1 }>
+                        <img src={img1}/>
+                    </div>
                 </Carousel.Item>
                 <Carousel.Item interval={5000}>
-                    <img src={img2} className={ styles.item }/>
+                    <div className={ styles.item1 }>
+                        <img src={img2}/>
+                    </div>
                 </Carousel.Item>
             </Carousel>
-        </div>
+        // </div>
     )
 }
