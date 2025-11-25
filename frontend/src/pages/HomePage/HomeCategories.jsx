b550b79b78b7a7381d4ac2f4b59eeba2b1aed0b8
Gonzalo Silvero <gonzalosilvero02@gmail.com>
2025-11-25 14:36:22 -0300

    Agrego imagenes de prueba

--- frontend/src/pages/HomePage/HomeCategories.jsx
+++ frontend/src/pages/HomePage/HomeCategories.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import {CategoriesCard}  from "../../components/CategoriesCard.jsx";
 import styles from "../../styles/HomeCategories.module.css"
-import img1 from "../../assets/img1.png";
-// import img2 from "../../assets/img2.jpg";
+import streatware from "../../assets/streatware.jpg";
+import casual from "../../assets/casual.jpg";
+import vintage from "../../assets/vintage.jpg";
+import sport from "../../assets/sport.jpg";
+
 
 export const HomeCategories = () => {
     return (
         <div className={ styles.container }>
-            <CategoriesCard text="hola" img={img1} alt="categoria"/>
-            <CategoriesCard text="hola" img={img1} alt="categoria"/>
-            <CategoriesCard text="hola" img={img1} alt="categoria"/>
-            <CategoriesCard text="hola" img={img1} alt="categoria"/>
+            <CategoriesCard text="streatware" img={streatware} alt="streatware"/>
+            <CategoriesCard text="casual" img={casual} alt="casual"/>
+            <CategoriesCard text="vintage" img={vintage} alt="vintage"/>
+            <CategoriesCard text="sport" img={sport} alt="sport"/>
         </div>
     )
 }
